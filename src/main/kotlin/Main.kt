7d4c42e683fe5a0e16dac4e00bfdfab179ce9d91
yoelexe <karmahucents@gmail.com>
2022-05-18 13:30:03 -0500

    new configures

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import kotlin.math.pow
+
+// es la entrada y se autoinvoca
 fun main(){
     // Boolean -> 1bit false/true
     // Bytes -> 8bit -127 to 128
@@ -30,4 +33,71 @@ fun main(){
 
     name = "Martina"
     println("My name is $name")
+
+    // suma
+    println(2+2)
+    // resta
+    println(2-2)
+    // división
+    println(2/2)
+    // modulos
+    println(2%2)
+    // multiplicación
+    println(2*2)
+    // potencia
+    println(2.0.pow(2))
+
+    // Operación matematica
+    println( 1 + (9 / 3) * 2)
+
+    // comparación e igualdad operadores
+    println(2 == 2)
+    println(2 != 2)
+    println(2 < 2)
+    println(2 > 2)
+    println(2 <= 2)
+    println(2 >= 2)
+
+    // Operadores de asignación
+    var anyNumber: Int = 0
+    anyNumber += 10
+    anyNumber -= 2
+    anyNumber *= 2
+    anyNumber /= 2
+    anyNumber %= 2
+    anyNumber++
+    anyNumber--
+    println(anyNumber)
+
+    var isTrue : Boolean = true
+    var isFalse : Boolean = false
+    // Operadores booleanso logicos
+    // AND
+    println(isTrue && isFalse)
+    // OR
+    println(isTrue || isFalse)
+    // NOT
+    println(!isTrue)
+
+    helloWorld()
+    var num : Int = dobletheValue( 2)
+    println(num)
+
+    favouriteFoodPrompt("Karen", "salchipapa")
+    favouriteFoodPrompt()
+}
+
+fun helloWorld(){
+    println("Hello world")
+}
+
+// retorna el doble de un valor, entra un entero y sale un entero
+//                          entrada    salida
+fun dobletheValue(numberValue: Int) : Int{
+    return numberValue * 2
+}
+
+// anynomuas -> por defecto lo asignas
+fun favouriteFoodPrompt(name: String = "Anonymous", favouriteFood: String = "Pizza"): Unit{
+    println("$name's favourite food is $favouriteFood")
 }
